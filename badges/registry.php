6ff68eb0506a04ae9961a60da99d1daef6490d4d
David Hoppe <dh@smpmedia.net>
2026-09-08 12:15:56 +0200

    Refactor release

--- badges/registry.php
+++ badges/registry.php
@@ -5,27 +5,23 @@ return [
         'title'   => 'NO AfD',
         'alt'     => 'NO AfD',
         'style'   => 'bold',
-        'current' => 'v0.1',
     ],
 
     'menschenwuerde' => [
         'title'   => 'Menschenwürde. Keine AfD.',
         'alt'     => 'Menschenwürde. Keine AfD.',
         'style'   => 'editorial',
-        'current' => 'v0.1',
     ],
 
     'demokratie' => [
         'title'   => 'Demokratie statt AfD.',
         'alt'     => 'Demokratie statt AfD.',
         'style'   => 'bold',
-        'current' => 'v0.1',
     ],
 
     'faschismus' => [
         'title'   => 'Gemeinsam gegen Faschismus.',
         'alt'     => 'Gemeinsam gegen Faschismus.',
         'style'   => 'editorial',
-        'current' => 'v0.1',
     ],
 ];
