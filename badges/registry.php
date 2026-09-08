e6a6db717315dc7b4b3b495b1b968843509d8b66
David Hoppe <dh@smpmedia.net>
2026-09-08 11:28:04 +0200

    Add dummy badges for release process

---
+++ badges/registry.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    'no-afd' => [
+        'title'   => 'NO AfD',
+        'alt'     => 'NO AfD',
+        'style'   => 'bold',
+        'current' => 'v0.1',
+    ],
+
+    'menschenwuerde' => [
+        'title'   => 'Menschenwürde. Keine AfD.',
+        'alt'     => 'Menschenwürde. Keine AfD.',
+        'style'   => 'editorial',
+        'current' => 'v0.1',
+    ],
+
+    'demokratie' => [
+        'title'   => 'Demokratie statt AfD.',
+        'alt'     => 'Demokratie statt AfD.',
+        'style'   => 'bold',
+        'current' => 'v0.1',
+    ],
+
+    'faschismus' => [
+        'title'   => 'Gemeinsam gegen Faschismus.',
+        'alt'     => 'Gemeinsam gegen Faschismus.',
+        'style'   => 'editorial',
+        'current' => 'v0.1',
+    ],
+];
